976f690c7c1bf237be9760b02d853898e7682db2
Paul Xiong <paul.xiong.2007@gmail.com>
2020-05-11 22:14:57 -0700

    support pyton3.8

---
+++ mycervical3dot8.py
@@ -0,0 +1,210 @@
+"""TODO(mycervical): Add a description here."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import os
+
+import numpy as np
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets.public_api as tfds
+
+#Shared constants
+_MYCERVICA_IMAGE_SIZE = 100
+_MYCERVICA_IMAGE_SHAPE = (_MYCERVICA_IMAGE_SIZE, _MYCERVICA_IMAGE_SIZE, 3)
+
+# Shared constants
+#_CIFAR_IMAGE_SIZE = 100
+#_CIFAR_IMAGE_SHAPE = (_CIFAR_IMAGE_SIZE, _CIFAR_IMAGE_SIZE, 3)
+
+COLAB = False
+XIAMEN = False
+MACBOOK = True
+KAGGLE = False
+
+# TODO(mycervical): BibTeX citation
+_CITATION = """\
+  @paul.xiong.2007@gmail.com
+"""
+
+# TODO(mycervical):
+_DESCRIPTION = "The mycervical dataset consists of 10000 100x100 color"
+
+
+class Mycervical(tfds.core.GeneratorBasedBuilder):
+  """TODO(mycervical): Short description of my dataset."""
+  MANUAL_DOWNLOAD_INSTRUCTIONS = """\
+  manual_dir should contain onw file: CIFAR-10BINA.tar.gz
+  """
+  # TODO(mycervical): Set up version.
+  VERSION = tfds.core.Version("3.0.2")
+  
+  
+  def _info(self):
+    # TODO(mycervical): Specifies the tfds.core.DatasetInfo object
+    return tfds.core.DatasetInfo(
+        builder=self,
+        # This is the description that will appear on the datasets page.
+        description=_DESCRIPTION,
+        # tfds.features.FeatureConnectors
+        features=tfds.features.FeaturesDict({
+            "id": tfds.features.Text(),
+            "image": tfds.features.Image(shape=_MYCERVICA_IMAGE_SHAPE),
+            "label": tfds.features.ClassLabel(num_classes=10),
+        }),
+        # If there's a common (input, target) tuple from the features,
+        # specify them here. They'll be used if as_supervised=True in
+        # builder.as_dataset.
+        supervised_keys=("image","label"),
+        # Homepage of the dataset for documentation
+        homepage="https://www.cs.toronto.edu/~kriz/cifar.html",
+        citation=_CITATION,
+    )
+  @property
+  def _mycervical_info(self):
+    return MycervicalInfo(
+        name=self.name,
+        url="./data/cifar-10-binary.tar.gz",
+        train_files=[
+            "data_batch_1.bin", "data_batch_2.bin", "data_batch_3.bin",
+            "data_batch_4.bin", "data_batch_5.bin"
+        ],
+        test_files=["test_batch.bin"],
+        prefix="cifar-10-batches-bin/",
+        label_files=["batches.meta.txt"],
+        label_keys=["label"],
+    )
+
+  def _split_generators(self, dl_manager):
+    """Returns SplitGenerators."""
+    # TODO(mycervical): Downloads the data and defines the splits
+    # dl_manager is a tfds.download.DownloadManager that can be used to
+    # download and extract URLs
+    '''return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            # These kwargs will be passed to _generate_examples
+            gen_kwargs={},
+        ),
+    ]'''
+    #boostx : commented following line to skip download and extract .gz.zip file.
+    #train_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2012_img_train.tar')
+
+    #mycervical_path = dl_manager.download_and_extract(self.mycervical_path.url)
+    
+    if COLAB:
+      mycervical_path = "/content/simclr-2/data/downloads/extracted/"
+    elif XIAMEN:
+      mycervical_path = "/notebooks/Paul/cervical/simclr-2/data/downloads/extracted/"
+    elif MACBOOK:
+      mycervical_path = "/Volumes/Bo500G32MCache/Cervical/simclr-2/data/downloads/extracted/"
+    elif KAGGLE:
+     mycervical_path = "/kaggle/working/simclr-2/data/downloads/extracted/"
+
+    mycervical_info = self._mycervical_info
+
+    mycervical_path = os.path.join(mycervical_path, mycervical_info.prefix)
+
+    #dl_manager.manual_dir = mycervical_path
+    print(dl_manager.manual_dir)
+    
+    # Load the label names
+    for label_key, label_file in zip(mycervical_info.label_keys,
+                                     mycervical_info.label_files):
+      labels_path = os.path.join(mycervical_path, label_file)
+      with tf.io.gfile.GFile(labels_path) as label_f:
+        label_names = [name for name in label_f.read().split("\n") if name]
+      self.info.features[label_key].names = label_names
+
+    # Define the splits
+    def gen_filenames(filenames):
+      for f in filenames:
+        yield os.path.join(mycervical_path, f)
+
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs={
+                "split_prefix": "train_",
+                "filepaths": gen_filenames(cifar_info.train_files)
+            }),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={
+                "split_prefix": "test_",
+                "filepaths": gen_filenames(cifar_info.test_files)
+            }),
+    ]
+
+  def _generate_examples(self, split_prefix, filepaths):
+    """Yields examples."""
+    # TODO(mycervical): Yields (key, example) tuples from the dataset
+    #yield 'key', {}
+    """Generate CIFAR examples as dicts.
+
+    Shared across CIFAR-{10, 100}. Uses self._cifar_info as
+    configuration.
+
+    Args:
+      split_prefix (str): Prefix that identifies the split (e.g. "tr" or "te").
+      filepaths (list[str]): The files to use to generate the data.
+
+    Yields:
+      The cifar examples, as defined in the dataset info features.
+    """
+    label_keys = self._mycervical_info.label_keys
+    index = 0  # Using index as key since data is always loaded in same order.
+    for path in filepaths:
+      for labels, np_image in _load_data(path, len(label_keys)):
+        record = dict(zip(label_keys, labels))
+        # Note: "id" is only provided for the user convenience. To shuffle the
+        # dataset we use `index`, so that the sharding is compatible with
+        # earlier versions.
+        record["id"] = "{}{:05d}".format(split_prefix, index)        
+        record["image"] = np_image
+        yield index, record
+        index += 1
+
+class MycervicalInfo(collections.namedtuple("_MycervicalInfo", [
+    "name",
+    "url",
+    "prefix",
+    "train_files",
+    "test_files",
+    "label_files",
+    "label_keys",
+])):
+  """Contains the information necessary to generate a CIFAR dataset.
+
+  Attributes:
+    name (str): name of dataset.
+    url (str): data URL.
+    prefix (str): path prefix within the downloaded and extracted file to look
+      for `train_files` and `test_files`.
+    train_files (list<str>): name of training files within `prefix`.
+    test_files (list<str>): name of test files within `prefix`.
+    label_files (list<str>): names of the label files in the data.
+    label_keys (list<str>): names of the label keys in the data.
+  """
+  
+
+def _load_data(path, labels_number=1):
+  """Yields (labels, np_image) tuples."""
+  with tf.io.gfile.GFile(path, "rb") as f:
+    data = f.read()
+  offset = 0
+  max_offset = len(data) - 1
+  while offset < max_offset:
+    labels = np.frombuffer(data, dtype=np.uint8, count=labels_number,
+                           offset=offset).reshape((labels_number,))
+    # 1 byte per label, 1024 * 3 = 3072 bytes for the image.
+    offset += labels_number
+    img = (np.frombuffer(data, dtype=np.uint8, count=30000, offset=offset)
+           .reshape((3, _MYCERVICA_IMAGE_SIZE, _MYCERVICA_IMAGE_SIZE))
+           .transpose((1, 2, 0))
+          )
+    #offset += 3072
+    offset += 30000
+    yield labels, img
